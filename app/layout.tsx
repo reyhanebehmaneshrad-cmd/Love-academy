761b5c1ab9376040ff5d7c921b624abef05b8e7f
reyhanebehmaneshrad-cmd <reyhanebehmaneshrad@gmail.com>
2026-08-14 15:49:14 +0330

    Add RootLayout component with metadata for layout

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "آکادمی عشق و صمیمیت",
+  description: "مرجعی برای یادگیری مهارت‌های ارتباطی",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
